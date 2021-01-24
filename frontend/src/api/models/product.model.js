06f2a90bda1fcd245fc3921f256ecf604e86fa27
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-01-24 12:32:29 +0200

    frontend: remove prettier && fiex some warnings

--- frontend/src/api/models/product.model.js
+++ frontend/src/api/models/product.model.js
@@ -1,7 +1,9 @@
-export default ProductModel = {
+const ProductModel = {
   name: String,
   price: Number,
   image: String,
   url: String,
   store: String,
 };
+
+export default ProductModel;
--- frontend/src/pages/ProductsPage/components/ProductsList/ProductsList.vue
+++ frontend/src/pages/ProductsPage/components/ProductsList/ProductsList.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="products-grid p-grid">
-    <Product v-for="product of products" :key="product.name" class="p-col-12 p-md-3 p-lg-3" :product="product" />
+    <Product
+      v-for="product of products"
+      :key="product.name"
+      class="p-col-12 p-md-3 p-lg-3"
+      :product="product"
+    />
   </div>
 </template>
 
@@ -13,7 +18,10 @@ export default {
     Product,
   },
   props: {
-    products: Array,
+    products: {
+      type: Array,
+      required: true,
+    },
   },
 };
 </script>
